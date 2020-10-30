ea624aed1982da3db239b0cc8631f7b7dfce1d39
zhengjiapeng <zhengjiapeng@coocaa.com>
2020-10-30 14:33:00 +0800

    refactor(babel): 新增babel配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 
 import pkg from './package.json'
 
@@ -41,5 +42,5 @@ export default {
             banner
         }
     ],
-    plugins: [commonjs(), resolve(), terser()]
+    plugins: [babel(), commonjs(), resolve(), terser()]
 }
